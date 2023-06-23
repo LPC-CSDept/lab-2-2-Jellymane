39d9e27e9dec9e088cfd21887faa5761a2037e53
Smokey 8 <jacobramsdell316@gmail.com>
2023-06-22 21:38:39 -0700

    Salary Lab 2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
-    workhours = 50
+   
+    workhours = int(input('Enter your workhours: '))
     reg_hours = 40
     reg_rate = 18.25
     ov_rate = 27.78
     
+
    ##################################################
    # Code your program here
    ##################################################
@@ -12,9 +14,9 @@ def main():
     regular_wage = reg_hours * reg_rate
     total_wage = regular_wage + overtime_wage
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
-    print(f"Total wage : {total_wage:.2f}")
+    print(f"Regular hours is {reg_hours} Regular Charge is {regular_wage}")
+    print(f"Overtime hours is {overtime} Overtime Charge is {overtime_wage:.2f}")
+    print(f"Total wage is {total_wage:.2f}")
 
    ##################################################
    # Do not delete the return statement
